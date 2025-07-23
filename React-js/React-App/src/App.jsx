0c4988850e206bfb0463f78e8572f5ddb24d336d
Gautam Gohil <gohilgautam2405@gmail.com>
2025-07-23 19:18:18 +0530

    React First Project

---
+++ React-js/React-App/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello, I am Gohil Gautam And i am a mern stack developer</h1>
+    </>
+  )
+}
+
+export default App
---
+++ React-js/first-react-app/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1>My Intro</h1>,
+    <h2>My Name Gautam. I am a Marn Stack developer.I am am 23 Year old.</h2>
+  );
+}
+
+export default App;
